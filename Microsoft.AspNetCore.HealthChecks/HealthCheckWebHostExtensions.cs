12b00dbfc5ffd935413fc735f400691b1ef67925
Jorge Fernández Sánchez <jorge.fernandez@encamina.com>
2018-12-24 21:05:04 +0000

    --

---
+++ Microsoft.AspNetCore.HealthChecks/HealthCheckWebHostExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace Microsoft.AspNetCore.HealthChecks
+{
+    using System;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.HealthChecks;
+
+    public static class HealthCheckWebHostExtensions
+    {
+        private const int DEFAULT_TIMEOUT_SECONDS = 300;
+
+        public static void RunWhenHealthy(this IWebHost webHost)
+        {
+            webHost.RunWhenHealthy(TimeSpan.FromSeconds(DEFAULT_TIMEOUT_SECONDS));
+        }
+
+        public static void RunWhenHealthy(this IWebHost webHost, TimeSpan timeout)
+        {
+            var healthChecks = webHost.Services.GetService(typeof(IHealthCheckService)) as IHealthCheckService;
+
+            var loops = 0;
+            do
+            {
+                var checkResult = healthChecks.CheckHealthAsync().Result;
+                if (checkResult.CheckStatus == CheckStatus.Healthy)
+                {
+                    webHost.Run();
+                    break;
+                }
+
+                System.Threading.Thread.Sleep(1000);
+                loops++;
+
+            } while (loops < timeout.TotalSeconds);
+        }
+    }
+}
